6a1c99c19c67767c9ad6db2a2590120ebe1db74b
ayush <trivediayush491112@gmail.com>
2020-06-11 14:39:26 +0530

    input field completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 class App extends Component {
   render() {
     return (
       <div>
-        Hello from main App
-        <TodoInput />
+        <TodoList />
       </div>
     );
   }
---
+++ src/components/TodoList.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import TodoForm from "./TodoForm";
+
+export default class TodoList extends Component {
+  state = {
+    todos: [],
+  };
+  addTodo = (todo) => {
+    this.setState({
+      todos: [todo, ...this.state.todos],
+    });
+  };
+  render() {
+    return (
+      <div>
+        <TodoForm onSubmit={this.addTodo} />
+        {JSON.stringify(this.state.todos)}
+      </div>
+    );
+  }
+}
